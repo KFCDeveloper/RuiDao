9fa6e0d5947b3fdbfbd2763daecec25412438d3d
ydy <978531051@qq.com>
2019-05-10 11:57:05 +0800

    new

--- SpringCoreTest/src/main/java/TestDI.java
+++ SpringCoreTest/src/main/java/TestDI.java
@@ -5,12 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestDI {
     public static void main(String[] args) {
-        //1. get Spring IOC container
+        /*//1. get Spring IOC container
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         //2. get TestService instance from container
         TestService instance = context.getBean(TestService.class);
         //if we can get a xx@xx, we can make sure dependencies is injected by Spring
-        instance.outputDependency();
+        instance.outputDependency();*/
+        testExampleBean();
 
     }
 
--- SpringCoreTest/src/main/java/com/neuedu/springtestcore/ExampleBean.java
+++ SpringCoreTest/src/main/java/com/neuedu/springtestcore/ExampleBean.java
@@ -1,15 +1,68 @@
 package com.neuedu.springtestcore;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class ExampleBean {
     private int years = 4000;
     private String ultimateAnswer = "aaa";
 
-    public ExampleBean(int years, String ultimateAnswer) {
+    //typed collection
+    private List<String> list;
+    private Set<String> set;
+    //第一个是String 第二个什么都可以
+    private Map<String,String>map;
+
+//    public ExampleBean(int years, String ultimateAnswer) {
+//        this.years = years;
+//        this.ultimateAnswer = ultimateAnswer;
+//    }
+
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public void setYears(int years) {
         this.years = years;
+    }
+
+    public void setUltimateAnswer(String ultimateAnswer) {
         this.ultimateAnswer = ultimateAnswer;
     }
 
     public void outPutInfo(){
         System.out.println(this.years+"\t"+this.ultimateAnswer);
+
+        //Iterate list
+        for(String item:list){
+            System.out.println(item);
+        }
+        System.out.println("================================");
+        //iterator set //foreach is implemented by iterator,so set can use foreach
+        for (String s : set) {
+            System.out.println(s);
+        }
+        System.out.println("================================");
+
+        //iterate map
+        //Map是一个接口，Entry是一个嵌套接口
+        Set<Map.Entry<String,String>> entries = map.entrySet();
+        Iterator<Map.Entry<String,String>> it2 = entries.iterator();
+        while (it2.hasNext()){
+            Map.Entry<String,String>entry = it2.next();
+            System.out.println(entry.getKey()+"\t"+entry.getValue());
+        }
+
     }
 }
